56220336c996c5456ddac4179c8ca2e022f16889
재범 서 <gleldzm3212@kpu.ac.kr>
2019-05-29 12:36:35 +0900

    기본 윈도우 생성
    창 생성

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import tkinter.ttk
+import http.client
+import urllib
+
+#일단 창 생성하기
+window=Tk()
+window.title("피바다")
+window.geometry("800*600+100+100")
+window.resizable(False,False)
+#기본 윈도우 생성끝
+
+
+
+window.mainloop()
